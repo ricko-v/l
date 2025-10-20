32b649a6a3aecf65233d3c79d4d69dbe03342413
ricko-v <veriyantoricko302@gmail.com>
2025-10-20 11:08:31 +0700

    feat: get config cross-platform

--- utils/get-config.js
+++ utils/get-config.js
@@ -1,21 +1,25 @@
+import path from "node:path";
+import { pathToFileURL } from "node:url";
 import { getCurrentDir } from "./get-current-dir.js";
 
 export const getConfig = async () => {
   const currentDir = getCurrentDir();
+
+  const parentDir = path.dirname(currentDir);
+
   try {
-    const { l } = await import(
-      `${currentDir
-        .split("/")
-        .slice(0, currentDir.split("/").length - 1)
-        .join("/")}/l.js`
-    );
+    const lPath = path.join(parentDir, "l.js");
+
+    const { l } = await import(pathToFileURL(lPath).href);
+
     if (!l.prefix || !l.profile || !l.region) {
-      console.log("Invalid config in l.js");
+      console.error("❌ Invalid config in l.js");
       process.exit(1);
     }
+
     return l;
   } catch (error) {
-    console.log("No l.js file found in current directory");
+    console.error("⚠️ No l.js file found in parent directory");
     process.exit(1);
   }
 };
